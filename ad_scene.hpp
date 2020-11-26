4b685bdc35297b5f83dd6a412c2b0f397c15ab7c
Nick Stathas <nistath@mit.edu>
2020-11-26 14:53:30 -0500

    Initial commit

---
+++ ad_scene.hpp
@@ -0,0 +1,13 @@
+#include "json/single_include/nlohmann/json.hpp"
+#include <fstream>
+#include <iostream>
+
+/**
+ * author   Arjun Albert
+ * email    aalbert@mit.edu
+ * date     11/26/2020
+**/
+
+using json = nlohmann::json;
+
+std::vector<std::string> entities;
---
+++ ads_kge.cpp
@@ -0,0 +1,68 @@
+#include "ad_scene.hpp"
+
+/**
+ * author   Arjun Albert
+ * email    aalbert@mit.edu
+ * date     11/26/2020
+**/
+
+
+json read_json(std::string file_name)
+{
+    std::ifstream file_in(file_name);
+    json json_out;
+    file_in >> json_out;
+    return json_out;
+}
+
+
+std::string get_scene_description(json element)
+{
+    std::string element_description;
+    element_description = element["description"];
+    return element_description;
+}
+
+
+std::vector<std::string> get_objects_from_description(std::string description)
+{
+    std::string delimiter = ",";
+    std::vector<std::string> objects;
+
+    size_t pos = 0;
+    std::string token;
+    while ((pos = description.find(delimiter)) != std::string::npos) {
+        token = description.substr(0, pos);
+        objects.push_back(token);
+        description.erase(0, pos + delimiter.length());
+    }
+    return objects;
+}
+
+
+bool is_new_entity(std::string object)
+{
+    return false;
+}
+
+
+int main()
+{
+    
+    json j = read_json("scene.json");
+    std::vector<std::string> descriptions;
+    for (auto& element : j) {
+        std::string description = get_scene_description(element);
+        descriptions.push_back(description);
+    }
+    
+
+    for (auto& description : descriptions) {
+        std::vector<std::string> objects = get_objects_from_description(description);
+        for (auto& object : objects){
+            std::cout << object;
+        }
+        std::cout << std::endl;
+    }
+    
+}
